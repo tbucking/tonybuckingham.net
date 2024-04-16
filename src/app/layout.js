9b66df8a3cb7066dd8ada468d7a48d6848d907bb
Tony Buckingham <tony@zeitcode.com>
2024-04-16 13:34:20 -0500

    add GA4

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,6 @@
 import { Inter } from "next/font/google";
+import Script from "next/script";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +13,18 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-NVP1LXWH8X" />
+        <Script id="google-analytics">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NVP1LXWH8X');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image src="/img/bear-01.png" alt="Bear" width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto'}} />
+      <Image src="/img/bear-01.png" alt="Bear" priority width={0} height={0} sizes="100vw" style={{ width: '100%', height: 'auto'}} />
     </main>
   );
 }
